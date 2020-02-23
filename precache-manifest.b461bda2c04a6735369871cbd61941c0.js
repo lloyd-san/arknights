aa749fdff6de55e81f2e9eb7257c7834a54880fc
Lloyd-san <lloyd.san.cs@gmail.com>
2020-02-23 12:31:20 +0100

    Updates

---
+++ precache-manifest.b461bda2c04a6735369871cbd61941c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd23a2689bc1f1920dc005fa269cbf5",
+    "url": "/arknights/index.html"
+  },
+  {
+    "revision": "4629ce5afb6545eca992",
+    "url": "/arknights/static/css/main.833452de.chunk.css"
+  },
+  {
+    "revision": "bb204108df1cddff9ba7",
+    "url": "/arknights/static/js/2.947517ca.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/arknights/static/js/2.947517ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4629ce5afb6545eca992",
+    "url": "/arknights/static/js/main.424f3fec.chunk.js"
+  },
+  {
+    "revision": "7d95d869a5f743b3f7b5",
+    "url": "/arknights/static/js/runtime-main.fe882b37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35a6399696be3a54efa6f87420c26c96.js"
+  "/arknights/precache-manifest.b461bda2c04a6735369871cbd61941c0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arknights/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
