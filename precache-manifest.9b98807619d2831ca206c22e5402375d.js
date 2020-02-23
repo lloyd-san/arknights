5e1802e49513e943edc7b74de06265bf7d8570b3
Lloyd-san <lloyd.san.cs@gmail.com>
2020-02-23 01:54:35 +0100

    Updates

---
+++ precache-manifest.9b98807619d2831ca206c22e5402375d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb937d4dc1d50cb1c2ac29602d0232bf",
+    "url": "/arknights/index.html"
+  },
+  {
+    "revision": "d4133def34f93ee7de55",
+    "url": "/arknights/static/css/main.833452de.chunk.css"
+  },
+  {
+    "revision": "bb204108df1cddff9ba7",
+    "url": "/arknights/static/js/2.947517ca.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/arknights/static/js/2.947517ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4133def34f93ee7de55",
+    "url": "/arknights/static/js/main.20f08be3.chunk.js"
+  },
+  {
+    "revision": "7d95d869a5f743b3f7b5",
+    "url": "/arknights/static/js/runtime-main.fe882b37.js"
+  }
+]);
